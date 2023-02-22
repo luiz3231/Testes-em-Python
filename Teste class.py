47bf03e110a81cbd185001c0ab1e2385a908d972
luiz3231 <124100662+luiz3231@users.noreply.github.com>
2023-02-22 16:17:56 -0300

    Teste class Cliente

--- Teste class.py
+++ Teste class.py
@@ -1,8 +1,8 @@
 class Cliente:
-    def __init__(self, nome:str, data_nascimento:str, cpf:str):
+    def __init__(self, nome:str, data_nascimento:str, CPF:str):
         self.nome = nome
         self.data_nascimento = data_nascimento
-        self.cpf = cpf
+        self.CPF = CPF
 
     DEVE = 0
     def comprou(self, valor:float):
@@ -14,24 +14,24 @@ class Cliente:
 
     
 if __name__ == '__main__':
-    Emily = Cliente('Emily','15/09/2007','123.456.789-25')
+    Ana = Cliente('Ana','15/09/2007','123.456.789-25')
     Luiz = Cliente('Luiz Eduardo Rodrigues Santos', '31/10/2005', '149.019.496-75')
 
     Luiz.comprou(300)
     Luiz.pagou(5)
     
-    Emily.comprou(900)
-    Emily.pagou(200)
+    Ana.comprou(900)
+    Ana.pagou(200)
 
     
-    print(f'Cliente: {Emily.nome}')
-    print(f'Data de nascimento: {Emily.data_nascimento}')
-    print(f'CPF: {Emily.cpf}')
-    print(f'Deve o Valor de R${Emily.DEVE}')
+    print(f'Cliente: {Ana.nome}')
+    print(f'Data de nascimento: {Ana.data_nascimento}')
+    print(f'CPF: {Ana.CPF}')
+    print(f'Deve o Valor de R${Ana.DEVE}')
     print()
     print()
     print(f'Cliente: {Luiz.nome}')
     print(f'Data de nascimento: {Luiz.data_nascimento}')
-    print(f'CPF: {Luiz.cpf}')
+    print(f'CPF: {Luiz.CPF}')
     print(f'Deve o Valor de R${Luiz.DEVE}')
     
