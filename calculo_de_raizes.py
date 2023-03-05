3a5dd35b64fc965151a6f61a07b5f55ffbb93cc7
Luiz Eduardo Rodrigues Santos <124100662+luiz3231@users.noreply.github.com>
2023-03-05 18:12:51 -0300

    Calculo
    Este é um codigo bem basico que fiz para me auxiliar em um
    trabalho de matematica da escola.

---
+++ calculo_de_raizes.py
@@ -0,0 +1,24 @@
+"""
+Este é um codigo bem basico que fiz para me auxiliar em um
+trabalho de matematica da escola.
+
+Formulas
+Δ=b²-4ac
+x=-b-+√Δ 
+
+""" 
+import math
+a = int(input('A = '))
+b = int(input('B = '))
+c = int(input('C = '))
+delta = b**2-4*a*c
+if delta < 0:
+    print('SØ')
+else:
+    x = ((b * -1) + math.sqrt(delta)) / 2
+    print(f'Δ = {delta}')
+    if x < 0:
+        X = x * (-1)
+        print(f'x = {X}')
+    else:
+        print(f'x = {x}')
